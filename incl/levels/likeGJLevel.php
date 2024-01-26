3b9b6a3b388eeae343bc785918587b86431a68c0
arti-max <128210100+arti-max@users.noreply.github.com>
2024-01-26 23:06:41 +0300

    Create likeGJLevel.php

---
+++ incl/levels/likeGJLevel.php
@@ -0,0 +1,20 @@
+<?php
+chdir(dirname(__FILE__));
+include "../lib/connection.php";
+require_once "../lib/Lib.php";
+$gs = new Lib();
+
+if(!isset($_POST['levelID']))
+	exit(-1);
+
+$levelID = $_POST['levelID']);
+
+$query=$db->prepare("SELECT likes FROM levels WHERE $levelID = :levelID LIMIT 1");
+$query->execute([':levelID' => $levelID]);
+$likes = $query->fetchColumn();
+
+
+$query=$db->prepare("UPDATE levels SET likes = likes + 1 WHERE levelID = :levelID");
+$query->execute([':levelID' => $levelID]);
+echo "1";
+?>
