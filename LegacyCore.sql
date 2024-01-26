5d414abd78f83827d51b81089b4025e7253f4e9e
arti-max <128210100+arti-max@users.noreply.github.com>
2024-01-26 23:10:45 +0300

    Add files via upload

---
+++ LegacyCore.sql
@@ -0,0 +1,161 @@
+-- phpMyAdmin SQL Dump
+-- version 4.9.5deb2
+-- https://www.phpmyadmin.net/
+--
+-- Хост: localhost:3306
+-- Время создания: Янв 26 2024 г., 23:10
+-- Версия сервера: 8.0.35-0ubuntu0.20.04.1
+-- Версия PHP: 7.4.3-4ubuntu2.19
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- База данных: `LegacyCore`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `actions`
+--
+
+CREATE TABLE `actions` (
+  `ID` int NOT NULL,
+  `type` int NOT NULL DEFAULT '0',
+  `value` varchar(255) CHARACTER SET utf8mb3 COLLATE utf8mb3_unicode_ci NOT NULL DEFAULT '0',
+  `timestamp` int NOT NULL DEFAULT '0',
+  `value2` varchar(255) CHARACTER SET utf8mb3 COLLATE utf8mb3_unicode_ci NOT NULL DEFAULT '0',
+  `value3` int NOT NULL DEFAULT '0',
+  `value4` int NOT NULL DEFAULT '0',
+  `value5` int NOT NULL DEFAULT '0',
+  `value6` int NOT NULL DEFAULT '0',
+  `account` int NOT NULL DEFAULT '0'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3 COLLATE=utf8mb3_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `downloads`
+--
+
+CREATE TABLE `downloads` (
+  `id` int NOT NULL,
+  `levelID` int NOT NULL,
+  `uploadDate` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `levels`
+--
+
+CREATE TABLE `levels` (
+  `levelID` int NOT NULL,
+  `levelName` varchar(255) CHARACTER SET utf8mb3 COLLATE utf8mb3_unicode_ci NOT NULL,
+  `levelDesc` mediumtext CHARACTER SET utf8mb3 COLLATE utf8mb3_unicode_ci NOT NULL,
+  `userName` mediumtext CHARACTER SET utf8mb3 COLLATE utf8mb3_unicode_ci NOT NULL,
+  `levelVersion` int NOT NULL,
+  `gameVersion` int NOT NULL,
+  `audioTrack` int NOT NULL,
+  `levelLength` int NOT NULL,
+  `userID` int NOT NULL,
+  `secret` mediumtext CHARACTER SET utf8mb3 COLLATE utf8mb3_unicode_ci,
+  `likes` int NOT NULL DEFAULT '0',
+  `downloads` int NOT NULL DEFAULT '0',
+  `levelString` longtext CHARACTER SET utf8mb3 COLLATE utf8mb3_unicode_ci,
+  `udid` varchar(255) CHARACTER SET utf8mb3 COLLATE utf8mb3_unicode_ci NOT NULL,
+  `isFeatured` int NOT NULL DEFAULT '0',
+  `F_POS` int NOT NULL DEFAULT '0',
+  `difficulty` int NOT NULL DEFAULT '0',
+  `uploadDate` bigint NOT NULL,
+  `diffOverride` int DEFAULT '0'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `users`
+--
+
+CREATE TABLE `users` (
+  `userID` int NOT NULL,
+  `udid` varchar(100) CHARACTER SET utf8mb3 COLLATE utf8mb3_unicode_ci NOT NULL COMMENT 'extID -> Cvolton',
+  `userName` varchar(69) CHARACTER SET utf8mb3 COLLATE utf8mb3_unicode_ci NOT NULL DEFAULT 'undefined',
+  `IP` varchar(69) CHARACTER SET utf8mb3 COLLATE utf8mb3_unicode_ci NOT NULL DEFAULT '127.0.0.1',
+  `secret` varchar(69) CHARACTER SET utf8mb3 COLLATE utf8mb3_unicode_ci NOT NULL DEFAULT 'none'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Индексы сохранённых таблиц
+--
+
+--
+-- Индексы таблицы `actions`
+--
+ALTER TABLE `actions`
+  ADD PRIMARY KEY (`ID`),
+  ADD KEY `type` (`type`),
+  ADD KEY `value` (`value`),
+  ADD KEY `value2` (`value2`),
+  ADD KEY `timestamp` (`timestamp`);
+
+--
+-- Индексы таблицы `downloads`
+--
+ALTER TABLE `downloads`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Индексы таблицы `levels`
+--
+ALTER TABLE `levels`
+  ADD PRIMARY KEY (`levelID`);
+
+--
+-- Индексы таблицы `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`userID`);
+
+--
+-- AUTO_INCREMENT для сохранённых таблиц
+--
+
+--
+-- AUTO_INCREMENT для таблицы `actions`
+--
+ALTER TABLE `actions`
+  MODIFY `ID` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2785;
+
+--
+-- AUTO_INCREMENT для таблицы `downloads`
+--
+ALTER TABLE `downloads`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT для таблицы `levels`
+--
+ALTER TABLE `levels`
+  MODIFY `levelID` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT для таблицы `users`
+--
+ALTER TABLE `users`
+  MODIFY `userID` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
