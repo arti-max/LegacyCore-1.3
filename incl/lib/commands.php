c3b956539cba7aeb566e404700b4b0c1133c985a
arti-max <128210100+arti-max@users.noreply.github.com>
2024-02-23 16:00:21 +0300

    Update commands.php

--- incl/lib/commands.php
+++ incl/lib/commands.php
@@ -1,19 +1,16 @@
 <?php
 class Commands {
 
-    public static function Commands($udid, $comment, $levelID) {
+ 
+    public static function doCommands($udid, $comment, $levelID) {
 
 		include dirname(__FILE__)."/../lib/connection.php";
 		require_once "../lib/Lib.php";
 		$gs = new Lib();
+
 		$commentarray = explode(' ', $comment);
-		$uploadDate = time();
-
-        $query2 = $db->prepare("SELECT udid FROM levels WHERE levelID = :id");
-		$query2->execute([':id' => $levelID]);
-		$targetUDID = $query2->fetchColumn();
-
-        if(substr($comment,0,9) == '!featured' AND $gs->checkPermission($udid)){
+        //COMMANDS
+        if(substr($comment,0,9) == '!featured'){
 			$isFeatured = $commentarray[1];
             $FeaturePOS = $commentarray[2];
 
@@ -26,9 +23,11 @@ class Commands {
 
             $query = $db->prepare("UPDATE levels SET isFeatured=:isFeatured, F_POS=:F_POS WHERE levelID=:levelID");
             $query->execute([':isFeatured' => $isFeatured, ':F_POS' => $FeaturePOS, ':levelID' => $levelID]);
+            
             return true;
+        }
+        return false;
     }
-}
 
 
 }
