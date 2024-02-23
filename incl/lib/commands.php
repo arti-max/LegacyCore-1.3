7b4027abee237a8c9c1b370d2c3102ba022accfb
arti-max <128210100+arti-max@users.noreply.github.com>
2024-02-23 16:31:51 +0300

    Update commands.php

--- incl/lib/commands.php
+++ incl/lib/commands.php
@@ -1,16 +1,30 @@
 <?php
 class Commands {
+    public static function CheckPermission($udid) {
+        if(is_numeric($udid)) return false;
 
+		include __DIR__ . "/connection.php";
+		//isAdmin check
+		$query = $db->prepare("SELECT isAdmin FROM users WHERE udid = :udid");
+		$query->execute([':udid' => $udid]);
+		$isAdmin = $query->fetchColumn();
+		if($isAdmin == 1){
+			return 1;
+		}
+		
+		return false;
+    }
  
     public static function doCommands($udid, $comment, $levelID) {
 
 		include dirname(__FILE__)."/../lib/connection.php";
 		require_once "../lib/Lib.php";
+        require_once "../../bot/DSsend.php";
 		$gs = new Lib();
 
 		$commentarray = explode(' ', $comment);
         //COMMANDS
-        if(substr($comment,0,9) == '!featured'){
+        if(substr($comment,0,9) == '!featured' AND self::CheckPermission($udid)){
 			$isFeatured = $commentarray[1];
             $FeaturePOS = $commentarray[2];
 
@@ -23,7 +37,7 @@ class Commands {
 
             $query = $db->prepare("UPDATE levels SET isFeatured=:isFeatured, F_POS=:F_POS WHERE levelID=:levelID");
             $query->execute([':isFeatured' => $isFeatured, ':F_POS' => $FeaturePOS, ':levelID' => $levelID]);
-            
+
             return true;
         }
         return false;
