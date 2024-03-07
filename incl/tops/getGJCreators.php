9516bee112692387a1db40b115a8586cdc181330
arti-max <128210100+arti-max@users.noreply.github.com>
2024-03-07 12:27:01 +0300

    Update getGJCreators.php

--- incl/tops/getGJCreators.php
+++ incl/tops/getGJCreators.php
@@ -10,11 +10,7 @@ $query = $db->prepare($query);
 $query->execute([':stars' => $stars]);
 $result = $query->fetchAll();
 foreach($result as &$user){
-	if(is_numeric($user["udid"])){
-		$udid = $user["udid"];
-	}else{
-		$udid = 0;
-	}
+	$udid = $user["udid"];
 	$xx++;
 	$pplstring .= "1:".$user["userName"].":2:".$user["userID"].":13:".":6:".$xx.":9:".$user["icon"].":10:".$user["color1"].":11:".$user["color2"].":3:".$user["stars"].":8:".round($user["cp"],0,PHP_ROUND_HALF_DOWN).":4:".$user["demons"].":7:".$udid."|";
 }
